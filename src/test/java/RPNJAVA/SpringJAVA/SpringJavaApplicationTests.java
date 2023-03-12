f96fc206a3e238bd2223535ec0f3ae9083a52b9c
Arseni <shakalalexandr@mail.ru>
2023-03-12 22:15:34 +0300

    aa

---
+++ src/test/java/RPNJAVA/SpringJAVA/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package RPNJAVA.SpringJAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
